0792878d09495c5f02f2988ceec663c855b40006
zchen36 <chenzhiping1@gmail.com>
2015-02-25 22:57:46 -0500

    add story model

---
+++ db/migrate/20150226035628_create_stories.rb
@@ -0,0 +1,14 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.text :description
+      t.integer :point
+      t.integer :stage
+      t.references :project, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :stories, :projects
+  end
+end
