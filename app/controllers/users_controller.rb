141dde9552cd7915aeefef309e26688da231b2a3
zchen36 <chenzhiping1@gmail.com>
2015-02-27 17:01:41 -0500

    admin can delete develop, can't delete self and admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,10 +43,19 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation,:admin)
     end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
